908ba8fedcfef46b59d46953d4603ba867079ba5
Денис Чухліб <dchuhleb@icloud.com>
2022-02-02 20:11:20 +0200

    feat: task done

--- src/index.js
+++ src/index.js
@@ -35,10 +35,48 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '     ':  ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    let arrExpr = expr.split(''),
+     size = 2,
+     dotsArr = [],
+     pointsArr = [],  
+     size2 = 5,
+     dotOrSlashArr = [],
+     lettersArr = [],
+     wordsArr = [],
+     codeArr = [];
+ 
+  
+  for (let i= 0; i < arrExpr.length/size; i++){
+      dotsArr[i] = arrExpr.slice((i*size), (i*size) + size )
+      pointsArr[i] = dotsArr[i].join('');
+  }
+  
+  pointsArr.forEach(item => {
+     if (item === '00') {
+         dotOrSlashArr.push('');        
+      } else if (item === '10') {
+         dotOrSlashArr.push('.');
+      } else if ( item === '11') {
+         dotOrSlashArr.push('-');
+      } else {
+        dotOrSlashArr.push(' ');
+      }
+  })
+ 
+  for (let i = 0; i <dotOrSlashArr.length/size2; i++){
+    lettersArr[i] = dotOrSlashArr.slice((i*size2), (i*size2) + size2 );
+  }
+  lettersArr.forEach( item => {
+    wordsArr.push((item.join('')));
+  })
+  wordsArr.forEach(item => {
+    codeArr.push(MORSE_TABLE[item])
+  })
+return codeArr.join('');
 }
 
 module.exports = {
